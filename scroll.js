d289aa332b16a57a34a56aea122b100522b8cdf3
Victor Alves <victoralvescabral7@gmail.com>
2023-04-20 01:18:01 -0300

    Third Commit

--- scroll.js
+++ scroll.js
@@ -1,11 +1,14 @@
-const observer = new IntersectionObserver(function (entries) {
+const observer = new IntersectionObserver(
+  function (entries) {
     entries.forEach(function (section) {
       if (section.isIntersecting) {
-        section.target.className = 'active';
+        section.target.className = "active";
       }
     });
-  }, {threshold: .8});
-  
-  document.querySelectorAll('section').forEach(section => {
-    observer.observe(section);
-  });
+  },
+  { threshold: 0.8 }
+);
+
+document.querySelectorAll("section").forEach((section) => {
+  observer.observe(section);
+});
